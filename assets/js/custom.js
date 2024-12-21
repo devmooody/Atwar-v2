0ea165a847dc3f6b677daafb646102b7657c3429
devmooody <developer.mody@gmail.com>
2024-12-21 11:11:48 +0200

    edit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,22 +6,8 @@
 			$('body').delay(50).css({ 'overflow': 'visible' });
 		})
 		
-		/*---- Bottom To Top Scroll Script ---*/
-		$(window).on('scroll', function() {
-			var height = $(window).scrollTop();
-			if (height > 100) {
-				$('#back2Top').fadeIn();
-			} else {
-				$('#back2Top').fadeOut();
-			}
-		});
-		
-		$("#back2Top").on('click', function(event) {
-			event.preventDefault();
-			$("html, body").animate({ scrollTop: 0 }, "slow");
-			return false;
-		});
-		
+
+	
 		// Tooltip
 		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 		var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
