db5f39f5c60aeae6d71115727a3d3fd49542bf65
devmooody <developer.mody@gmail.com>
2025-01-08 03:25:55 +0200

    edit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -269,7 +269,7 @@
 	  autoplaySpeed:1400,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1200,
           settings: {
             slidesToShow: 1.75,
 			slidesToScroll: 1,
